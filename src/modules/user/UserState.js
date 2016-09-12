b1b75d9eaeda4f23c3c3444eaccca640dd90eac7
NealTaylor715 <neal.taylorjr@gmail.com>
2016-09-12 12:29:33 -0500

    (feat) add user creation function for sign up users

--- src/modules/user/UserState.js
+++ src/modules/user/UserState.js
@@ -1,5 +1,6 @@
 import {Map, fromJS} from 'immutable';
 import {loop, Effects} from 'redux-loop';
+import {createUser} from '../../services/backScratchService';
 
 // Initial state
 const initialState = Map({
@@ -9,12 +10,28 @@ const initialState = Map({
 
 // Actions
 const USER_FOUND = 'UserState/USER_FOUND';
+const POST_RESPONSE = 'UserState/POST_RESPONSE';
+const POST_REQUEST = 'UserState/POST_REQUEST';
 
 // Action creators
 export function onExistingUser(user) {
   return {
     type: USER_FOUND,
     payload: fromJS(user)
+  };
+}
+
+export function post(user) {
+  return {
+    type: POST_REQUEST,
+    payload: user
+  };
+}
+
+export async function submitNewUser(user) {
+  return {
+    type: POST_RESPONSE,
+    payload: await createUser(user)
   };
 }
 
@@ -26,6 +43,15 @@ export default function UserStateReducer(state = initialState, action = {}) {
       return state
         .set('value', action.payload);
 
+    case POST_REQUEST:
+      return loop(
+        state.set('loading', true),
+        Effects.promise(submitNewUser, action.payload)
+      );
+    case POST_RESPONSE:
+      return state
+        .set('loading', false)
+        .set('value', action.payload);
     default:
       return state;
   }
