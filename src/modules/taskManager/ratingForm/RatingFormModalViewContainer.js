4ad6f7916de8374c5ef413afec4ffdc8df01ccdf
Harvey Sanders <sanders.harvey@gmail.com>
2016-09-13 14:48:56 -0500

    (chore) move rating modal to correct folder

---
+++ src/modules/taskManager/ratingForm/RatingFormModalViewContainer.js
@@ -0,0 +1,9 @@
+import {connect} from 'react-redux';
+import {toJs} from 'immutable'; // eslint-disable-line no-unused-vars
+import RatingFormModalView from './RatingFormModalView';
+
+export default connect(
+  state => ({
+    userId: state.getIn(['user', 'value', 'userId'])
+  })
+)(RatingFormModalView);
